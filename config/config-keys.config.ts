4490f32d12ad98be9c1b72ea477adad34e80fb02
ICEatm <ICEatm@users.noreply.github.com>
2024-06-13 11:58:12 +0000

    💄 Applied linter

--- config/config-keys.config.ts
+++ config/config-keys.config.ts
@@ -2,19 +2,18 @@
  * Enum containing keys for configuration values related to paths.
  */
 export const enum ConfigKeys {
-    /**
-     * Key for the logs path configuration.
-     */
-    LOGS_PATH = 'default.paths.logsPath',
-  }
-  
   /**
-   * Enum containing keys for configuration values related to messages.
+   * Key for the logs path configuration.
    */
-  export const enum MessageKeys {
-    /**
-     * Key for the welcome message configuration.
-     */
-    INDEX_WELCOME = 'default.messages.indexWelcome',
-  }
-  
+  LOGS_PATH = 'default.paths.logsPath',
+}
+
+/**
+ * Enum containing keys for configuration values related to messages.
+ */
+export const enum MessageKeys {
+  /**
+   * Key for the welcome message configuration.
+   */
+  INDEX_WELCOME = 'default.messages.indexWelcome',
+}
