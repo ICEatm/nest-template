966f7deb497d4c2463c02772da6105ade526a8cd
ICEatm <violettruhe1@gmail.com>
2024-06-14 09:07:46 +0200

    ✨ Added /api as the prefix for all routes except for the index route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set `/api` as the global prefix
+  // except for the `/` index page
+  app.setGlobalPrefix('api', { exclude: ['/'] });
+
+  /**
+   * Enable versioning by setting it the type to uri
+   * To use this import `Version` from `@nestjs/common`
+   *
+   * To call it on the browser request
+   * http://localhost:<PORT>/api/v1/cats
+   *
+   * @example
+   * ```typescript
+   * import { Get, Version, Controller } from '@nestjs/common';
+   *
+   * @Controller('cats')
+   * export class IndexController {
+   *  @Get()
+   *  @Version('1')
+   *  getIndex() {
+   *    return 'Cats';
+   *  }
+   * }
+   * ```
+   */
+  app.enableVersioning({ type: VersioningType.URI });
+
   await app.listen(3000);
 }
 bootstrap();
