627a71e93155ace93c03ea43594303045bd28baf
ICEatm <ICEatm@users.noreply.github.com>
2024-06-20 12:29:35 +0000

    💄 Applied automatic linter

--- src/main.ts
+++ src/main.ts
@@ -71,7 +71,11 @@ async function bootstrap() {
       .build();
 
     const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-    SwaggerModule.setup((process.env.SWAGGER_BASE_PATH as string ?? '/docs'), app, swaggerDocument);
+    SwaggerModule.setup(
+      (process.env.SWAGGER_BASE_PATH as string) ?? '/docs',
+      app,
+      swaggerDocument,
+    );
   };
 
   setupSwagger(app);
